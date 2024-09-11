5dabf304a4911caddd3bf2d6241c5573f596b30a
m-mwangi <m.mwangi2@alustudent.com>
2024-09-11 10:48:05 -0700

    11-the_western_exchange.py

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Transpose matrix"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a matrix.
+
+    Args:
+        matrix: A numpy.ndarray.
+
+    Returns:
+        numpy.ndarray: The transposed matrix.
+    """
+    return matrix.T
