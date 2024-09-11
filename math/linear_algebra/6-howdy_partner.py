ad1deee2bb37319eecbc249f93c7dbbaeff1ce56
m-mwangi <m.mwangi2@alustudent.com>
2024-09-11 10:42:51 -0700

    6-howdy_partner.py

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays
+
+    Args:
+        arr1, arr2
+
+    Returns:
+        list: A new 2D list representing the concat of two arrays.
+
+    """
+    return arr1 + arr2
