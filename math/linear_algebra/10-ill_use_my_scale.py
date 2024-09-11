88e24f78e102335e4c988551a06dcd496ebb637c
m-mwangi <m.mwangi2@alustudent.com>
2024-09-11 10:47:01 -0700

    10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Numpy shape of array"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray.
+
+    Args:
+        matrix: A numpy.ndarray.
+
+    Returns:
+        tuple: The shape of the array as a tuple of integers.
+    """
+    return matrix.shape
