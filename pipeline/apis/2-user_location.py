e5edf1528090b776add9381f7afc93f5453fd478
m-mwangi <127857750+m-mwangi@users.noreply.github.com>
2025-02-26 17:46:58 +0000

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
 
 """
-This script uses the GitHub API to print the location of a specific user.
-"""
+This module contains a function that
+uses Github API to print location of
+specific users"""
 
 import requests
 import sys
 import time
 
-def print_location():
-    """Prints the location of a GitHub user from the API."""
-    
-    # Ensure a URL argument is provided
-    if len(sys.argv) < 2:
-        print("Usage: ./2-user_location.py <GitHub API URL>")
-        sys.exit(1)
+# get the url from string passed on the terminal
 
+# If the user doesn’t exist, print Not found
+# If the status code is 403, print Reset in X min
+# where X is the number of minutes from now and the value of X-Ratelimit-Reset
+# Your code should not be executed when the file is imported
+# (you should use if __name__ == '__main__':)
+
+
+def print_location():
+    """print location of user"""
     url = sys.argv[1]
     response = requests.get(url)
+    data = response.json()
 
     if response.status_code == 403:
-        rate_limit = int(response.headers.get('X-Ratelimit-Reset', 0))
+        rate_limit = int(response.headers.get('X-Ratelimit-Reset'))
         current_time = int(time.time())
         diff = (rate_limit - current_time) // 60
         print("Reset in {} min".format(diff))
-        return  # Stop execution
 
     elif response.status_code == 404:
         print("Not found")
-        return  # Stop execution
-
     elif response.status_code == 200:
-        data = response.json()
-        print(data.get('location', "No location available"))
+        print(data['location'])
+
 
 if __name__ == "__main__":
     print_location()
