57ffaa6eb5398e06c9134e1c6d30cc2961c36923
m-mwangi <m.mwangi2@alustudent.com>
2025-03-09 19:10:02 +0000

    14-country_users.sql

---
+++ pipeline/databases/14-country_users.sql
@@ -0,0 +1,7 @@
+-- Creates table users with requirements
+-- if table exists, script not fail
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US');
